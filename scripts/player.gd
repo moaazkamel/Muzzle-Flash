b57ea4fca03b9d4dd759b6639f4d96f8fe86fd9d
moaazkamel <moaaz.kamel.gber@gmail.com>
2026-05-20 02:50:56 +0300

    Refactor player.gd for improved shooting and input handling

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,12 @@ extends CharacterBody3D
 
 
 @export var SPEED = 7.0
+
+
+
 @export var JUMP_VELOCITY = 3.5
+
+
 @export var gravity = -9.8
 
 @export var sensitivity = 0.004 
@@ -21,6 +26,8 @@ extends CharacterBody3D
 
 
 var bullets_left = 40 
+
+
 var bullet = preload("res://scenes/area_3d.tscn")
 
 
@@ -37,9 +44,21 @@ func _ready() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
+
+
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * sensitivity)
+
+
+
+
+
+
+
+
 		camera_3d.rotate_x(-event.relative.y * sensitivity	)
+
+
 		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta: float) -> void:
@@ -55,10 +74,15 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_pressed("shoot") and bullets_left > 0:
 		if !gun_animation.is_playing():
+
+
+
 			gun_animation.play("shoot")
 			shoot()
 	if Input.is_action_just_pressed("reload"):
 		gun_animation.play("reload")
+
+
 		bullets_left = 40 
 	
 	
@@ -68,24 +92,44 @@ func _physics_process(delta: float) -> void:
 		
 		
 	var input_dir := Input.get_vector("left", "right", "up", "down")
+
+
+
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
+
+
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+
+
+
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 
 func damage():
 	health -= 1
+
+
+
 	pass
 func shoot():
+
+
 	
 	bullets_left -= 1 
 	var bullet_instance =bullet.instantiate()
+
+
 	bullet_instance.position = gun_raycast.global_position 
+
+
+
 	bullet_instance.transform.basis = gun_raycast.global_transform.basis
 	get_parent().add_child(bullet_instance)
 	
